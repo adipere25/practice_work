f30a6d3c6a174b9c6bcab8d635cb2d3175192793
adipere25 <adiperecollins25@gmail.com>
2025-10-23 17:13:38 +0100

    Add area calculations for circle, traingle, and rectangle

---
+++ shapes.py
@@ -0,0 +1,25 @@
+import math
+
+# Area of a Cicle
+
+# radius = float(input("Enter the radius of the circle: "))
+# area_circle = math.pi * radius ** 2
+# print(f"The area of the circle with radius {radius} is: {area_circle:.2f}")
+
+print("")  # just a blank line for better readability
+
+# Area of a Triangle
+# base = float(input("Enter the base of the triangle: "))
+# height = float(input("Enter the height of the triangle: "))
+# area_triangle = 0.5 * base * height
+# print(f"The area of the triangle with base {base} and height {height} is: {area_triangle:.2f}") 
+
+print("")
+
+# Area of a Rectangle
+length = float(input("Enter the length of the rectangle: "))
+breadth = float(input("Enter the breadth of the rectangle: "))
+area_rectangle = length * breadth
+print(f"The area of the rectangle with length {length} and breadth {breadth} is: {area_rectangle:.2f}")
+
+
